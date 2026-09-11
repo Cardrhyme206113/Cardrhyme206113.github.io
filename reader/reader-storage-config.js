2374b67ceb968c2edd39cfdc65183f8092ff3247
Cardrhyme <70390455+Cardrhyme206113@users.noreply.github.com>
2026-09-11 07:37:33 +0300

    Add files via upload

---
+++ reader/reader-storage-config.js
@@ -0,0 +1,15 @@
+// Generated storage routing. Viewer may be hosted at ANY GitHub Pages path.
+window.__READER_STORAGE__ = Object.freeze({
+  "version": 2,
+  "metadataRoot": "https://raw.githubusercontent.com/Cardrhyme206113/reader-storage-a/main/",
+  "roots": [
+    "https://raw.githubusercontent.com/Cardrhyme206113/reader-storage-a/main/",
+    "https://raw.githubusercontent.com/Cardrhyme206113/reader-storage-b/main/",
+    "https://raw.githubusercontent.com/Cardrhyme206113/reader-storage-c/main/"
+  ],
+  "bucketBoundaries": [
+    82,
+    163
+  ],
+  "revision": "8b38306945c5bf6c"
+});
